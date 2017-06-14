6b07bacc5b9b9bf2ca09727b54924b15506c94db
Oleksandr Veklych <alexandrveklich@gmail.com>
2017-06-14 22:48:03 +0300

    add test, update send_form view

--- random_winners/tests.py
+++ random_winners/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from django.test.client import Client
+
+from .views import send_form
+from .models import Person
+
 
 # Create your tests here.
+class TestAddPersons(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_add_new_name(self):
+        POST_DATA = {
+            "name": "Саша"
+        }
+
+        self.client.post('', data=POST_DATA)
+
+        self.assertTrue(Person.objects.all().count(), 1)
+        self.assertTrue(Person.objects.all()[0], 'Саша')
+
+    def test_format_name(self):
+        POST_DATA = {
+            "name": "сАШа"
+        }
+
+        self.client.post('', data=POST_DATA)
+
+        self.assertTrue(Person.objects.all().count(), 1)
+        self.assertTrue(Person.objects.all()[0], 'Саша')
+
+    def test_the_same_one_name(self):
+        POST_DATA = {
+            "name": "Саша"
+        }
+
+        self.client.post('', data=POST_DATA)
+        self.client.post('', data=POST_DATA)
+
+        self.assertTrue(Person.objects.all().count(), 1)
+
+    def test_add_more_names(self):
+        POST_DATA = {
+            "name": "Саша"
+        }
+
+        self.client.post('', data=POST_DATA)
+
+        POST_DATA.update({"name": "Петя"})
+        self.client.post('', data=POST_DATA)
+
+        self.assertTrue(Person.objects.all().count(), 2)
--- random_winners/views.py
+++ random_winners/views.py
@@ -1,16 +1,26 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render
 from .forms import PersonForm
+from .models import Person
 
 
 # Create your views here.
 def send_form(request):
-    if request.method == "POST" and request.is_ajax():
+    if request.method == "POST":
         form = PersonForm(request.POST)
         if form.is_valid():
-            form.save()
-        return JsonResponse({"Имя {} успешно добавлено!".format(form.cleaned_data['my_form_field_name'])})
+            name = form.cleaned_data['name'].lower()
+            name = name.capitalize()
+            if Person.objects.filter(name=name).exists():
+                return JsonResponse("Name {} is exist in DB".format(name), safe=False)
+            else:
+                form.save(commit=False)
+                form.name = name
+                form.save()
+            return JsonResponse("Name {} is successfully added!".format(form.cleaned_data['name']), safe=False)
     else:
         form = PersonForm()
     return render(request, "random_winners/index.html", {'form': form})
+
+
 
